b70aa469352e9e7db8b6ab915b415a4dd58ab4c6
Shivam Kumar <shivamkingkumar5@gmail.com>
2025-12-19 01:16:16 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -9,8 +9,8 @@ from langchain_core.messages import HumanMessage, SystemMessage
 # ======================================================
 # 👇 PASTE YOUR KEYS HERE
 # ======================================================
-os.environ["GROQ_API_KEY"] = "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"
-os.environ["TAVILY_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "GROQ_API_KEY"
+os.environ["TAVILY_API_KEY"] = "TAVILY_API_KEY"
 
 # --- MODELS ---
 # Main brain for chatting
@@ -83,4 +83,5 @@ def generate_response(user_input, file_bytes=None, file_type=None, history=[]):
     try:
         return llm_text.invoke(messages).content
     except Exception as e:
+
         return f"Error: {str(e)}"
